f261a78c190337b87cec063293c10aeb98896865
Abrahán Fernández <afernandez.nieto@atsistemas.com>
2021-03-07 18:51:36 +0100

    Subida Base proyecto 1

---
+++ Proyecto1/Base/CursoiOS-Proyecto1/CursoiOS-Proyecto1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CursoiOS-Proyecto1
+//
+//  Created by Abrahán Fernández on 28/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
